e535aaefa4f8e2a8856b6b01749d1f5ed6a7c6f4
Dillon Awes <dillonawes@gmail.com>
2024-02-08 15:27:18 -0600

    react init

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
+import React from "react";
+
 function App() {
   return (
-    <>
-      <h1>Test</h1>
-    </>
+    <header className="header">
+      <div className="logo">
+        <img src="logo.png" alt="Today I Learned Logo" />
+        <h1>Today I Learned</h1>
+      </div>
+      <button className="btn btn-large btn-open">Share a fact</button>
+    </header>
   );
 }
 
